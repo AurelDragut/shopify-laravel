9f30e7eb703c9a371eb1444ff81922a338a8bfcd
Aurel Dragut <aurel.dragut@gmail.com>
2019-02-12 21:51:14 +0200

    'aaaaa'

---
+++ app/Http/Controllers/ReportsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ReportsController extends Controller
+{
+    public function getReport() {
+        $client = new \Weengs\AmazonMwsClient(
+            env('AWS_ACCESS_KEY'),
+            env('MWS_CLIENT_SECRET'),
+            env('MWS_SELLER_ID'),
+            [],
+            env('MWS_AUTHORISATION_TOKEN'),
+            'https://mws.amazonservices.it'
+        );
+
+        foreach (Product::all() as $product) {
+            dd($product);
+        }
+    }
+}
