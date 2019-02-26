661823fe490e7e0dff6dd83e5f4c93f1af106aec
Aurel Dragut <aurel.dragut@gmail.com>
2019-02-26 08:44:30 +0200

    message

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index($response)
+    public function index($response=null)
     {
         $response->header('X-Frame-Options', '\'ALLOW-FROM https://fashiononduty.myshopify.com/');
         return view('welcome');
